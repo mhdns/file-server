dac7d8900c890b75866b7d6fd0808e72d8885908
Anas <muhd.anas@outlook.com>
2021-01-05 18:16:51 +0800

    implement user store

--- auth_service/auth/user_store.go
+++ auth_service/auth/user_store.go
@@ -28,21 +28,39 @@ func NewInMemoryUserStore() *InMemoryUserStore {
 
 // Save commits User into in-memory store
 func (store *InMemoryUserStore) Save(user *User) error {
-	return fmt.Errorf("error")
+	_, ok := store.Store[user.Email]
+	if ok {
+		return fmt.Errorf("user already exits")
+	}
+
+	store.Store[user.Email] = user
+	return nil
 }
 
 // Get retrieves a User based on email address and returns an error if user
 // doesn't exist
 func (store *InMemoryUserStore) Get(email string) (*User, error) {
-	return nil, fmt.Errorf("error")
+	if user, ok := store.Store[email]; ok {
+		return user, nil
+	}
+
+	return nil, fmt.Errorf("user not found")
 }
 
 // Update takes a pointer to the User and updates the details in the store
-func (store *InMemoryUserStore) Update(*User) error {
-	return fmt.Errorf("error")
+func (store *InMemoryUserStore) Update(user *User) error {
+	if user, ok := store.Store[user.Email]; ok {
+		store.Store[user.Email] = user
+		return nil
+	}
+	return fmt.Errorf("user not found")
 }
 
 // Delete removes a User from the store
 func (store *InMemoryUserStore) Delete(email string) error {
-	return fmt.Errorf("error")
+	if _, ok := store.Store[email]; ok {
+		delete(store.Store, email)
+		return nil
+	}
+	return fmt.Errorf("user not found")
 }
--- auth_service/auth/user_store_test.go
+++ auth_service/auth/user_store_test.go
@@ -15,37 +15,78 @@ func TestNewInMemoryUserStore(t *testing.T) {
 }
 
 func TestSave(t *testing.T) {
-	userStore, user, _ := createAndSaveUser(t)
+	userStore, user, _ := createUserAndStore(t)
 	err := userStore.Save(user)
 	require.NoError(t, err)
 	require.Equal(t, 1, len(userStore.Store))
 }
 
+func TestSaveDuplicate(t *testing.T) {
+	userStore, user, _ := createUserAndStore(t)
+	err := userStore.Save(user)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(userStore.Store))
+
+	err = userStore.Save(user)
+	require.Error(t, err)
+
+}
+
 func TestGet(t *testing.T) {
-	userStore, user, email := createAndSaveUser(t)
+	userStore, user, email := createUserAndStore(t)
+	err := userStore.Save(user)
+	require.NoError(t, err)
+
 	userFromStore, err := userStore.Get(email)
 	require.NoError(t, err)
 	require.Equal(t, user, userFromStore)
 }
 
+func TestGetUserMissing(t *testing.T) {
+	// Don't save user to check if error is returned when queried for the user
+	userStore, _, email := createUserAndStore(t)
+	_, err := userStore.Get(email)
+	require.Error(t, err)
+}
+
 func TestUpdate(t *testing.T) {
-	userStore, _, email := createAndSaveUser(t)
+	userStore, user, email := createUserAndStore(t)
+	err := userStore.Save(user)
+	require.NoError(t, err)
+
+	updatedUser := auth.NewUser(email, "random", "Jack Ma")
+	err = userStore.Update(updatedUser)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(userStore.Store))
+
+}
+
+func TestUpdateUserMissing(t *testing.T) {
+	userStore, _, email := createUserAndStore(t)
 
 	updatedUser := auth.NewUser(email, "random", "Jack Ma")
 	err := userStore.Update(updatedUser)
-	require.NoError(t, err)
-	require.Equal(t, 1, len(userStore.Store))
+	require.Error(t, err)
 
 }
 
 func TestDelete(t *testing.T) {
-	userStore, _, email := createAndSaveUser(t)
-	err := userStore.Delete(email)
+	userStore, user, email := createUserAndStore(t)
+	err := userStore.Save(user)
 	require.NoError(t, err)
-	require.Equal(t, 1, len(userStore.Store))
+
+	err = userStore.Delete(email)
+	require.NoError(t, err)
+	require.Equal(t, 0, len(userStore.Store))
 }
 
-func createAndSaveUser(t *testing.T) (*auth.InMemoryUserStore, *auth.User, string) {
+func TestDeleteUserMissing(t *testing.T) {
+	userStore, _, email := createUserAndStore(t)
+	err := userStore.Delete(email)
+	require.Error(t, err)
+}
+
+func createUserAndStore(t *testing.T) (*auth.InMemoryUserStore, *auth.User, string) {
 	userStore := auth.NewInMemoryUserStore()
 	email := "random@gmail.com"
 	password := "password"
