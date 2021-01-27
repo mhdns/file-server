b38b173555ceea8a8e063115f0546c2b958d4898
Anas <muhd.anas@outlook.com>
2021-01-27 15:29:38 +0800

    wrote basic test for file upload

--- file_service/file_server/file_server_test.go
+++ file_service/file_server/file_server_test.go
@@ -1,16 +1,24 @@
 package file_test
 
 import (
+	"context"
+	"net"
 	"testing"
 
 	"github.com/mhdns/web_server/file_service/file_pb"
 	file "github.com/mhdns/web_server/file_service/file_server"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 )
 
 func TestFileUpload(t *testing.T) {
-	_ = file.NewFileServer("/Users/anas/go/src/github.com/mhdns/web_server/tmp")
+	serverAddr := createFileServer(t, "/Users/anas/go/src/github.com/mhdns/web_server/tmp")
+	client := createFileClient(t, serverAddr)
 
-	_ = &file_pb.FileUploadRequest{
+	stream, err := client.FileUpload(context.Background())
+	require.NoError(t, err)
+
+	req := &file_pb.FileUploadRequest{
 		Data: &file_pb.FileUploadRequest_Metadata{
 			Metadata: &file_pb.Metadata{
 				Filename: "test_file.html",
@@ -18,8 +26,35 @@ func TestFileUpload(t *testing.T) {
 				Filesize: 50,
 			}},
 	}
+
+	err = stream.Send(req)
+	require.NoError(t, err)
+
+	_, err = stream.CloseAndRecv()
+	require.NoError(t, err)
 }
 
 func TestFileDelete(t *testing.T) {
 
+}
+
+func createFileClient(t *testing.T, addrString string) file_pb.FileServiceClient {
+	conn, err := grpc.Dial(addrString, grpc.WithInsecure())
+	require.NoError(t, err)
+
+	return file_pb.NewFileServiceClient(conn)
+}
+
+func createFileServer(t *testing.T, homeDirectory string) string {
+	server := file.NewFileServer(homeDirectory)
+
+	grpcServer := grpc.NewServer()
+
+	file_pb.RegisterFileServiceServer(grpcServer, server)
+
+	li, err := net.Listen("tcp", ":0")
+	require.NoError(t, err)
+
+	go grpcServer.Serve(li)
+	return li.Addr().String()
 }
