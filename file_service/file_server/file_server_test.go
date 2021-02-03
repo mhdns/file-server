4cf30f873e036db7e44c6a8df1b71ab7a24ea8d8
Anas <muhd.anas@outlook.com>
2021-02-03 17:31:13 +0800

    complete test on uploading file

--- file_service/file_server/file_server_test.go
+++ file_service/file_server/file_server_test.go
@@ -1,8 +1,11 @@
 package file_test
 
 import (
+	"bufio"
 	"context"
+	"io"
 	"net"
+	"os"
 	"testing"
 
 	"github.com/mhdns/web_server/file_service/file_pb"
@@ -12,30 +15,59 @@ import (
 )
 
 func TestFileUpload(t *testing.T) {
-	serverAddr := createFileServer(t, "/Users/anas/go/src/github.com/mhdns/web_server/tmp")
+	serverAddr := createFileServer(t, "/Users/anas/go/src/github.com/mhdns/web_server/file_service/tmp_destination")
 	client := createFileClient(t, serverAddr)
 
 	stream, err := client.FileUpload(context.Background())
 	require.NoError(t, err)
 
+	file, err := os.Open("/Users/anas/go/src/github.com/mhdns/web_server/tmp_source/test_file")
+	require.NoError(t, err)
+	defer file.Close()
+
+	fileInfo, err := file.Stat()
+	require.NoError(t, err)
+
+	reader := bufio.NewReader(file)
+	buffer := make([]byte, 1024)
+
 	req := &file_pb.FileUploadRequest{
 		Data: &file_pb.FileUploadRequest_Metadata{
 			Metadata: &file_pb.Metadata{
-				Filename: "test_file.html",
-				Filetype: "html",
-				Filesize: 50,
+				Filename: fileInfo.Name(),
+				Filetype: "",
+				Filesize: int32(fileInfo.Size()),
 			}},
 	}
 
 	err = stream.Send(req)
 	require.NoError(t, err)
 
+	for {
+		_, err := reader.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+
+		chunk := &file_pb.FileUploadRequest{
+			Data: &file_pb.FileUploadRequest_Chunk{
+				Chunk: &file_pb.File{
+					Chunk: buffer,
+				},
+			},
+		}
+
+		err = stream.Send(chunk)
+		require.NoError(t, err)
+	}
+
 	_, err = stream.CloseAndRecv()
 	require.NoError(t, err)
 }
 
 func TestFileDelete(t *testing.T) {
-	serverAddr := createFileServer(t, "/Users/anas/go/src/github.com/mhdns/web_server/tmp")
+	serverAddr := createFileServer(t, "/Users/anas/go/src/github.com/mhdns/web_server/tmp_source")
 	clientServer := createFileClient(t, serverAddr)
 	filePath := ""
 	req := &file_pb.FileDeleteRequest{
@@ -54,7 +86,7 @@ func createFileClient(t *testing.T, addrString string) file_pb.FileServiceClient
 }
 
 func createFileServer(t *testing.T, homeDirectory string) string {
-	server := file.NewFileServer(homeDirectory)
+	server := file.NewFileServer(homeDirectory, 1000)
 
 	grpcServer := grpc.NewServer()
 
